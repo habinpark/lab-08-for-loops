30625d8dafdb2103156be0ff178c93844521a3b3
Habin Park <HabinPark@community.int>
2022-03-28 16:16:01 -0400

    party

--- party.py
+++ party.py
@@ -1,7 +1,10 @@
 
 partyCountDown = int(input("How long 'til the party?: "))
 
+if partyCountDown < 0:
+    print("PARTY NOW!!!")
 for i in range(partyCountDown, 0, -1):
     print(i)
+    if i == 1:
+        print("PARTY TIME!!!")
 
-print("PARTY TIME!!!")
